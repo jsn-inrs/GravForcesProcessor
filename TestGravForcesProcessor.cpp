e5c3d3da95c5d5b2ab9029de3106d059a5bad764
Jonathan Savin <jonathan.savin@inrs.fr>
2024-09-18 16:55:27 +0200

    Code simplified to the max to have a step by step progression...

--- TestGravForcesProcessor.cpp
+++ TestGravForcesProcessor.cpp
@@ -7,27 +7,26 @@
 //=============================================================================
 // INCLUDES
 //=============================================================================
+#include <iostream>
 #include "GravForcesProcessor.h"
-
-//=============================================================================
-// STATICS
-//=============================================================================
 
 //=============================================================================
 // DEFINES
 //=============================================================================
-#define OSIM_ARM_MODEL      "./Resources/test0.osim"
+#define OSIM_ARM_MODEL      "./resources/test0.osim"
 #define TEST 10
 
 int main()
 {
     // Set global log level to debug
-    spdlog::set_level(spdlog::level::debug);
-    spdlog::debug("Running main TestGravForcesProcessor...");
+//    spdlog::set_level(spdlog::level::debug);
+//    spdlog::debug("Running main TestGravForcesProcessor...");
 
     // Create the GravForcesProcessor instance
     GravForcesProcessor myGravTest = GravForcesProcessor(TEST);
-    spdlog::debug("Gravity Force Test: {}", myGravTest.getGravTorques());
+//    spdlog::debug("Gravity Force Test: {}", myGravTest.getGravTorques());
+//    std::cout << "Gravity Force Test: " << myGravTest.getGravTorques() << std::endl;
 
-    spdlog::debug("Done !");
+//    spdlog::debug("Done !");
+    std::cout << "Done !" << std::endl;
 }
